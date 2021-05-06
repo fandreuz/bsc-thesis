20a29d94e74388ca7c9ec654aacea10e5e373805
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2021-05-07 00:27:15 +0200

    gather random data. plot tree graph. inizio parte performance

--- experiments/time/src/timer.py
+++ experiments/time/src/timer.py
@@ -1,6 +1,8 @@
+from itertools import product
+
 if __name__ == "__main__":
     ns = [10, 20, 50, 100, 250, 500, 1000, 2500, 5000, 10000, 11000]
-    numbers = [100, 100, 100, 100, 100, 100, 25, 25, 25, 15, 15, 10, 5]
+    ratios = range(1,10)
 
     import numpy as np
     import networkx as nx
@@ -10,26 +12,17 @@ if __name__ == "__main__":
     # 1: FBA
     # 2: n of edges
 
-    n = 2
-    graph = nx.DiGraph()
-    graph.add_nodes_from(range(1,n+1))
-    graph.add_edges_from([(i, n // 2 + 2 * i - 1) for i in range(1, n+1)])
-    graph.add_edges_from([(n//2+i, 2*i-1) for i in range(n//2+1, n+1)])
-    graph.add_edges_from([(n // 4 + 1, 2 * i - 1) for i in range(1, n // 4 + 1)])
+    ratio = 2
 
-    print(graph.edges)
-
-    results = np.zeros((len(ns), 4))
-    for n_idx, n, number in zip(range(len(ns)), ns, numbers):
+    results = np.zeros((len(ns), len(ratios), 4))
+    for n_idx, n in enumerate(ns):
         print('doing n={}'.format(n))
+        for r_idx, ratio in enumerate(ratios):
+            print('\tr={}'.format(ratio))
+            p = ratio / n
+            graph = nx.fast_gnp_random_graph(n, p, directed=True)
 
-        graph = nx.DiGraph()
-        graph.add_nodes_from(range(1,n+1))
-        graph.add_edges_from([(i, n // 2 + 2 * i - 1) for i in range(1, n+1)])
-        graph.add_edges_from([(n//2+i, 2*i-1) for i in range(n//2+1, n+1)])
-        graph.add_edges_from([(n//4+1, 2*i-1) for i in range(1,n//4+1)])
-
-        setup = """\
+            setup = """\
 import networkx as nx
 from bisimulation_algorithms import paige_tarjan, dovier_piazza_policriti
 
@@ -39,12 +32,11 @@ graph.add_edges_from({})
 
 import sys
 sys.setrecursionlimit(20000)""" .format(n,list(graph.edges))
+            import timeit
+            pta = timeit.timeit('paige_tarjan(graph, is_integer_graph=False)', setup=setup, number=10) / 10
+            fba = timeit.timeit('dovier_piazza_policriti(graph, is_integer_graph=False)', setup=setup, number=10) / 10
 
-        import timeit
-        pta = timeit.timeit('paige_tarjan(graph, is_integer_graph=False)', setup=setup, number=number) / number
-        fba = timeit.timeit('dovier_piazza_policriti(graph, is_integer_graph=False)', setup=setup, number=number) / number
+            results[n_idx, r_idx] = np.array([pta, fba, len(graph.nodes), len(graph.edges)])
 
-        results[n_idx] = np.array([pta, fba, len(graph.nodes), len(graph.edges)])
-
-        print('\t{},{}'.format(pta, fba))
-    np.save('bisi/hopcroft/result_second_class.npy', results)
+            print('\t\t{},{}'.format(pta, fba))
+    np.save('bisi/random/result.npy', results)
